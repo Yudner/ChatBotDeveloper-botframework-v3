e049f9b9464ddc3aafc86c65435037ae90f9977b
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2018-05-25 00:45:07 -0500

    Bot Framework integration with QnA Maker
    Bot Framework integration with QnA Maker

---
+++ 8. Bot Framework and QnA Maker Integration/SampleBotQnAMaker/SampleBotQnAMakerSolution/Dialogs/RootDialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace SampleBotQnAMakerSolution.Dialogs
+{
+    [Serializable]
+    [QnAMaker(authKey: "You authKey", knowledgebaseId: "you knowledgebaseId", defaultMessage: "Sin respuesta", scoreThreshold: 0.5, top: 1, endpointHostName: "you HostName")]
+    public class RootDialog : QnAMakerDialog
+    {
+
+    }
+}
