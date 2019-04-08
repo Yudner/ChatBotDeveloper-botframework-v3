3b2218fede62294351304fa804fb71b3ce99c6f0
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2019-04-07 21:40:14 -0500

    Dependency injection Bot Framework

--- 11. Inyección de dependencias Bot Framework/ChatbotSolution/Chatbot/Dialogs/RootDialog.cs
+++ 11. Inyección de dependencias Bot Framework/ChatbotSolution/Chatbot/Dialogs/RootDialog.cs
@@ -8,7 +8,7 @@ using Microsoft.Bot.Builder.Luis.Models;
 
 namespace Chatbot.Dialogs
 {
-    [LuisModel(modelID: "6a8e8ca2-7ae4-4f0c-862a-00ccfa44147d", subscriptionKey: "fd08d1ddfc494ba8abea8761cc806593", domain: "centralus.api.cognitive.microsoft.com", threshold: 0.5)]
+    [LuisModel(modelID: "", subscriptionKey: "", domain: "", threshold: 0.5)]
     [Serializable]
     public class RootDialog : LuisDialog<object>
     {
