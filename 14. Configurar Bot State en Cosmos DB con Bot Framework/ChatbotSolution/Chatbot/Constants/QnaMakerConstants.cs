efa8bd651a3576133f72320a3eb53feba2e33b46
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2019-04-28 01:07:30 -0500

    Bot State with Cosmos DB

--- 14. Configurar Bot State en Cosmos DB con Bot Framework/ChatbotSolution/Chatbot/Constants/QnaMakerConstants.cs
+++ 14. Configurar Bot State en Cosmos DB con Bot Framework/ChatbotSolution/Chatbot/Constants/QnaMakerConstants.cs
@@ -7,9 +7,9 @@ namespace Chatbot.Constants
 {
     public class QnaMakerConstants
     {
-        public const string Host = "https://chatbot-qnamaker.azurewebsites.net/qnamaker";
-        public const string KnowledgeBaseId = "44056855-251c-48d2-9679-7bb9cde7e696";
-        public const string EndPointKey = "85d43489-f579-467f-984c-2cdbaf4be202";
+        public const string Host = "";
+        public const string KnowledgeBaseId = "";
+        public const string EndPointKey = "";
 
         public const string FormatJson = "application/json";
         public const string AnswerNotFound = "no good match found in kb.";
