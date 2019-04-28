194943c9005e4bf7b236aff81c2655491a17293d
Yudner Tayson Paredes Flores <yudner.paredes@hotmail.com>
2019-04-28 01:04:05 -0500

    Dependency injection Bot Framework

--- 11. Inyección de dependencias Bot Framework/ChatbotSolution/Chatbot/Constants/QnaMakerConstants.cs
+++ 11. Inyección de dependencias Bot Framework/ChatbotSolution/Chatbot/Constants/QnaMakerConstants.cs
@@ -7,9 +7,9 @@ namespace Chatbot.Constants
 {
     public class QnaMakerConstants
     {
-        public const string Host = "https://chatbot-qnamaker.azurewebsites.net/qnamaker";
-        public const string KnowledgeBaseId = "b4b607fd-55cc-4092-8dc3-d030e5480c54";
-        public const string EndPointKey = "2ffcf71b-4072-4647-90c3-a878e6d2d29f";
+        public const string Host = "";
+        public const string KnowledgeBaseId = "";
+        public const string EndPointKey = "";
 
         public const string FormatJson = "application/json";
         public const string AnswerNotFound = "no good match found in kb.";
